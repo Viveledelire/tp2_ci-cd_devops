97c3a3a9640d1e96d9daf11f3eb291df186f597b
Anaëlle <97473758+Viveledelire@users.noreply.github.com>
2024-04-04 01:39:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,12 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-#@app.route('/')
-#def home():
-#    return "Welcome to the Flask CI/CD Demo"
-
 @app.route('/')
 def home():
-    title = "Flask CI/CD Demo"
-    subtitle = "A simple example of deploying a Flask app with CI/CD"
-    return render_template('index.html', title=title, subtitle=subtitle)
+    #title = "Flask CI/CD Demo"
+    #subtitle = "A simple example of deploying a Flask app with CI/CD"
+    #return render_template('index.html', title=title, subtitle=subtitle)
+    return "Welcome to the Flask CI/CD Demo"
 
 @app.route('/new-deployment')
 def new_deployment():
